da490cab42337f0676ef84ed9fe4e2ffd5f7f5da
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-07-14 23:35:04 -0300

    feat: cria entidade Medico

---
+++ api/src/main/java/med/voll/api/entity/Medico.java
@@ -0,0 +1,27 @@
+package med.voll.api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.api.enums.Especialidade;
+
+@Entity
+@Table(name = "medicos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+}
