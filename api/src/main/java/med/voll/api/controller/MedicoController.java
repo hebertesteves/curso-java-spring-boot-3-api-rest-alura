545732220bb280a101f9592018bce82986db0f8d
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-07-16 10:26:52 -0300

    feat: implementa funcionalidade de salvar os dados do medico no banco de dados

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.dto.request.DadosCadastroMedico;
+import med.voll.api.entity.Medico;
+import med.voll.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dadosCadastroMedico) {
-        System.out.println(dadosCadastroMedico);
+        medicoRepository.save(new Medico(dadosCadastroMedico));
     }
 }
