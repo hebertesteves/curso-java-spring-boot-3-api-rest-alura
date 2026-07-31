9ed2d7abd39b519f6284ec26d362b75a9375187b
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-07-31 08:23:28 -0300

    feat: cria endpoint GET para listar todos os medicos

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,13 +3,14 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.dto.request.DadosCadastroMedico;
+import med.voll.api.dto.request.DadosCadastroPaciente;
+import med.voll.api.dto.response.DadosListagemMedico;
 import med.voll.api.entity.Medico;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +23,13 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico) {
         medicoRepository.save(new Medico(dadosCadastroMedico));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listarTodosOsMedicos() {
+        return medicoRepository.findAll()
+                .stream()
+                .map(DadosListagemMedico::new)
+                .toList();
     }
 }
