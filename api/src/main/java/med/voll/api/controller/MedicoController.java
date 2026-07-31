bcad27e2d3f201042f3ffc91a6be418689cc695a
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-07-31 08:35:35 -0300

    feat: implementa Paginacao no endpoint de buscar todos os medicos

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,11 +3,12 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.dto.request.DadosCadastroMedico;
-import med.voll.api.dto.request.DadosCadastroPaciente;
 import med.voll.api.dto.response.DadosListagemMedico;
 import med.voll.api.entity.Medico;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,10 +27,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listarTodosOsMedicos() {
-        return medicoRepository.findAll()
-                .stream()
-                .map(DadosListagemMedico::new)
-                .toList();
+    public Page<DadosListagemMedico> listarTodosOsMedicos(Pageable paginacao) {
+        return medicoRepository.findAll(paginacao)
+                .map(DadosListagemMedico::new);
     }
 }
