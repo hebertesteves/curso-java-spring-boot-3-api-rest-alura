c96c5c36225c7feffbe5a728eda8535d0a428789
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-07-31 08:15:34 -0300

    feat: cria DadosCadastroPaciente

---
+++ api/src/main/java/med/voll/api/dto/request/DadosCadastroPaciente.java
@@ -0,0 +1,14 @@
+package med.voll.api.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosCadastroPaciente(
+        @NotBlank String nome,
+        @NotBlank @Email String email,
+        @NotBlank String telefone,
+        @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}") String cpf,
+        @NotNull DadosEndereco endereco) {
+}
