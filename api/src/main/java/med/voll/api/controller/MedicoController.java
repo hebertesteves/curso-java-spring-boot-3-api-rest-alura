88500460970a48eb6e046b759627372ca84708bf
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-08-03 08:28:26 -0300

    feat: implementa anotacao PageableDefault para definir os valores padroes

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,6 +9,7 @@ import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,7 +28,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listarTodosOsMedicos(Pageable paginacao) {
+    public Page<DadosListagemMedico> listarTodosOsMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return medicoRepository.findAll(paginacao)
                 .map(DadosListagemMedico::new);
     }
