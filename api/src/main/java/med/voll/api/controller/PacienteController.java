a847266a24a5144f22f0d7b0c00ceaf01729f41c
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-08-03 08:33:50 -0300

    feat: cria endpoint GET para trazer todos os pacientes

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -3,13 +3,14 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.dto.request.DadosCadastroPaciente;
+import med.voll.api.dto.response.DadosListagemPaciente;
 import med.voll.api.entity.Paciente;
 import med.voll.api.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -22,5 +23,11 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         pacienteRepository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listarTodosOsPacientes(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+        return pacienteRepository.findAll(paginacao)
+                .map(DadosListagemPaciente::new);
     }
 }
