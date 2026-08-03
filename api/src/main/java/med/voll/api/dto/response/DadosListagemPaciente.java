40a115919478331c99f685c6b9620e11a13689ba
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-08-03 19:55:19 -0300

    refactor: refatora para retornar id do paciente em DadosListagemPaciente

--- api/src/main/java/med/voll/api/dto/response/DadosListagemPaciente.java
+++ api/src/main/java/med/voll/api/dto/response/DadosListagemPaciente.java
@@ -3,10 +3,11 @@ package med.voll.api.dto.response;
 import med.voll.api.entity.Paciente;
 
 public record DadosListagemPaciente(
+        Long id,
         String nome,
         String email,
         String cpf) {
     public DadosListagemPaciente(Paciente paciente) {
-        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
     }
 }
